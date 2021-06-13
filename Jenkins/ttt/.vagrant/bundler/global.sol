532caf654cfcf4cbf78292b4651ac3a28586e247
Mohamed Salah MEDDEB <medsalah.meddeb@gmail.com>
2021-06-14 00:42:33 +0100

    ADD LAb 5 - Provision

--- Jenkins/ttt/.vagrant/bundler/global.sol
+++ Jenkins/ttt/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.29.0"]]],"checksum":"f14f9a06b3a1730bff62e6be123354c341a18b25cf7af60c1d6d9f452889c0f1","vagrant_version":"2.2.16"}
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"a17c0554d18cedf5c223ac87beaf9d54f0e56b8bbb7d9150ae26d1395f27fec1","vagrant_version":"2.2.16"}
